b79a42f32dec8880ac519bbfae917258bf0652e6
natali2001 <49942113+natali2001@users.noreply.github.com>
2020-04-20 12:18:45 +0300

    00

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     redirect_to root_url
   end
   def create
-   ? user = User.find_by(email: params[:session][:email].downcase)
-    if ?user && ?user.authenticate(params[:session][:password])
+   user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       log_in ?user
       params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
       redirect_to ?user
